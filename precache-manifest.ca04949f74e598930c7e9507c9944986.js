723dba5ec1d878efc44ec7ab39aa350a3014a8c3
Dev91205 <devwriter91205@gmail.com>
2025-06-20 15:56:04 +0530

    Updates

---
+++ precache-manifest.ca04949f74e598930c7e9507c9944986.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80b2ccd05e854c2ed6d19d3ae2225be9",
+    "url": "/footprint-calculator/index.html"
+  },
+  {
+    "revision": "bada4260cdb5e1cdf4ba",
+    "url": "/footprint-calculator/static/css/main.b6307453.chunk.css"
+  },
+  {
+    "revision": "8a671afecc2e9e7f4f0d",
+    "url": "/footprint-calculator/static/js/2.10b60433.chunk.js"
+  },
+  {
+    "revision": "bada4260cdb5e1cdf4ba",
+    "url": "/footprint-calculator/static/js/main.2a3b7b6f.chunk.js"
+  },
+  {
+    "revision": "dda46ff1c378a707faf9",
+    "url": "/footprint-calculator/static/js/runtime~main.b5728a59.js"
+  },
+  {
+    "revision": "0dfd45339b56e0f2dd01a1fa82be1044",
+    "url": "/footprint-calculator/static/media/title.0dfd4533.png"
+  },
+  {
+    "revision": "563016f2aefcdf30abfd250c0374d397",
+    "url": "/footprint-calculator/static/media/title_img.563016f2.png"
+  },
+  {
+    "revision": "8083940700fc6018bc7904c123b0afac",
+    "url": "/footprint-calculator/static/media/trivia.80839407.png"
+  }
+]);
